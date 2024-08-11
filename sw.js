ce6d4da66bf4770d424092a3b55bd16517084f16
phyothura <phyoethura75@gmail.com>
2024-08-11 22:17:54 +0700

    update latest

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e6e2b067b7d9d12e3f58.js"
+    "url": "webpack-runtime-da7f33573ae83fb2e507.js"
   },
   {
     "url": "framework-fbf2011d6c2b83ba228a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8977ae3037190ed67548262a100bab2e"
+    "revision": "a6ed18a69cccf0d61e3575c92b96e708"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-27c66d5dcd0cba7d37a1.js"
